1c434c6937b186583f7a4e3b63949b87fc8d985e
toypipi <275688469@qq.com>
2024-12-19 17:34:47 +0800

    配置 Axios 相应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,4 +6,18 @@ const service = axios.create({
   timeout: 5000 // request timeout
 })
 
+/**
+ * 响应拦截器：
+ * 服务端返回数据之后，前端 .then 方法之前拦截
+ */
+
+service.interceptors.response.use((response) => {
+  const { success, message, data } = response.data
+  if (success) {
+    return data
+  }
+  // TODO:业务请求错误
+  return Promise.reject(new Error(message))
+})
+
 export default service
