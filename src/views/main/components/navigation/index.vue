cd40f717a73e550ce4e3c3c7d6efe7ef0b6ce0a5
toypipi <275688469@qq.com>
2024-12-19 17:35:20 +0800

    实现 navigationBar

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -1,10 +1,22 @@
 <template>
-  <mobile-navigation-vue v-if="isMobileTerminal" />
+  <mobile-navigation-vue v-if="isMobileTerminal" :data="categoryData" />
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { isMobileTerminal } from '@/utils/flexible'
 import mobileNavigationVue from './mobile/index.vue'
+
+import { getCategory } from '@/api/category'
+
+const categoryData = ref([])
+const getCategoryData = async () => {
+  const { categories } = await getCategory()
+  categoryData.value = categories
+  console.log(categoryData.value)
+}
+
+getCategoryData()
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,15 +1,25 @@
 <template>
-  <div>我是移动端下的 navigation</div>
+  <div class="bg-white sticky top-0 left-0 z-10">
+    <ul
+      class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
+    >
+      <li
+        v-for="item in data"
+        :key="item.id"
+        class="shrink-0 px-1.5 py-0.5 z-10 duration-200"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
-import { getCategory } from '@/api/category'
-const getCategoryData = async () => {
-  const res = await getCategory()
-  console.log(res)
-}
-
-getCategoryData()
+// 在 vite 构建的项目中，可以直接使用 defineProps 来接收父组件传递的 props
+defineProps({
+  data: {
+    type: Array,
+    Required: true
+  }
+})
 </script>
-
-<style lang="scss" scoped></style>
