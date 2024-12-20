9834cd0dd573750ea188b9e5d5a244ac3aba4783
toypipi <275688469@qq.com>
2024-12-20 14:06:44 +0800

    调整 fontSize 基准值

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { useREM } from './utils/flexible'
 import './styles/index.scss'
 
+useREM()
 createApp(App).use(router).mount('#app')
--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -3,6 +3,7 @@ import { PC_DEVICE_WIDTH } from '@/constants'
 import { useWindowSize } from '@vueuse/core'
 
 const { width } = useWindowSize()
+
 /**
  *
  * @returns 判断当前是否为移动设备，判断依据屏幕宽度是否小于一个指定宽度（1280）
@@ -10,3 +11,23 @@ const { width } = useWindowSize()
 export const isMobileTerminal = computed(() => {
   return width.value < PC_DEVICE_WIDTH
 })
+
+/**
+ * 动态指定 rem 基准值，最大为 40px
+ * 根据用户的屏幕宽度，进行一些计算，把计算出来的值赋给 html 的跟标签作为 fontsize 大小
+ */
+export const useREM = () => {
+  // 定义做大的 fontsize
+  const MAX_FONT_SIZE = 40
+  // 监听 html 文档被解析完成的事件
+  document.addEventListener('DOMContentLoaded', () => {
+    // 拿到 html 标签
+    const html = document.querySelector('html')
+    // 计算 fontsize，根据屏幕宽度/10
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE : fontSize
+    console.log(fontSize)
+    // 把计算出来的 fontsize 赋给 html 的根标签
+    html.style.fontSize = fontSize + 'px'
+  })
+}
