1ffa3d93b919e9ab889929b0c288ef01d7c86b5a
toypipi <275688469@qq.com>
2024-12-24 15:39:10 +0800

    完成 header 模块布局

--- src/views/layout/components/header/index.vue
+++ src/views/layout/components/header/index.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>Header</div>
+  <div class="w-full bg-white border-b border-b-zinc-200 px-2 py-1">
+    <div class="flex items-center">
+      <img
+        src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
+        alt=""
+        srcset=""
+        class="h-4 cursor-pointer mr-2"
+        @click="onToHome"
+      />
+      <headerSearchVue class="mr-1" />
+      <headerThemeVue class="mr-1" />
+      <headerMyVue />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import headerSearchVue from './header-search/index.vue'
+import headerThemeVue from './header-theme.vue'
+import headerMyVue from './header-my.vue'
+import { useRouter } from 'vue-router'
+
+// router 等于 VUE2 中的 this.$router
+const router = useRouter()
+const onToHome = () => {
+  router.push('/')
+}
+</script>
 
 <style lang="scss" scoped></style>
