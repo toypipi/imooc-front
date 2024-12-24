b8e7c06b32beb9413062ab2a6528670fb4c639da
toypipi <275688469@qq.com>
2024-12-24 15:10:12 +0800

    处理 PC 段基础架构

---
+++ src/views/layout/components/header/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Header</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="h-screen">
+    <headerVue class="h-header" />
+    <mainVue class="h-main" />
+    <floatingVue />
+  </div>
+</template>
+
+<script setup>
+import headerVue from './components/header/index.vue'
+import mainVue from './components/main.vue'
+import floatingVue from './components/floating.vue'
+</script>
+
+<style lang="scss" scoped></style>
