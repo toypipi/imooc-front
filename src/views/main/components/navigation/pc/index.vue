1940688872345dd7f56d760b33cd29c10a0adecb
toypipi <275688469@qq.com>
2025-01-08 11:29:49 +0800

    PC 端导航条选中状态处理和展开状态切换处理

--- src/views/main/components/navigation/pc/index.vue
+++ src/views/main/components/navigation/pc/index.vue
@@ -2,6 +2,7 @@
   <div class="bg-white sticky top-0 left-0 w-full z-10">
     <ul
       class="w-[800px] relative flex flex-wrap justify-center overflow-x-auto px-[10px] py-1 text-xs text-zinc-600 duration-300 overflow-hidden mx-auto"
+      :class="[isOpenCategory ? 'h-[206px]' : 'h-[56px]']"
     >
       <!-- 箭头展示 -->
       <div
@@ -9,15 +10,20 @@
       >
         <m-svg-icon
           class="w-1 h-1"
-          name="unfold"
+          :name="isOpenCategory ? 'fold' : 'unfold'"
           fillClass="text-zinc-900"
+          @click="triggerState"
         ></m-svg-icon>
       </div>
       <!-- item -->
       <li
-        v-for="item in $store.getters.categories"
+        v-for="(item, index) in $store.getters.categories"
         :key="item.id"
         class="shrink-0 px-1.5 py-0 z-10 duration-200 text-zinc-900 text-base font-bold h-4 leading-4 cursor-pointer hover:bg-zinc-200 rounded mr-1 mb-1"
+        :class="[
+          currentCategoryIndex === index ? 'bg-zinc-200 text-zinc-900' : ''
+        ]"
+        @click="onItemClick(index)"
       >
         {{ item.name }}
       </li>
@@ -25,6 +31,25 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+/**
+ * 状态切换处理
+ */
+const isOpenCategory = ref(false)
+const triggerState = () => {
+  isOpenCategory.value = !isOpenCategory.value
+}
+
+/**
+ * 选中状态处理
+ */
+
+const currentCategoryIndex = ref(0)
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
+</script>
 
 <style lang="scss" scoped></style>
