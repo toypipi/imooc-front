e6772b49f3ef51e84757fb0cabda86d2c40dcd9e
toypipi <275688469@qq.com>
2025-01-10 11:27:03 +0800

    实现主题跟随系统

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,30 +1,50 @@
-import { THEME_LIGHT, THEME_DARK } from '@/constants'
+import { THEME_LIGHT, THEME_DARK, THEME_SYSTEM } from '@/constants'
 import { watch } from 'vue'
 import store from '../store'
 
-console.log(store.state.themeType)
+/**
+ * 监听系统主题变化
+ */
+let matchMedia
+const watchSystemThemeChange = () => {
+  // 仅需一次初始化
+  if (matchMedia) return
+  matchMedia = window.matchMedia('(prefers-color-scheme: dark)')
+  // 监听系统主题变化
+  matchMedia.onchange = () => {
+    changeTheme(THEME_SYSTEM)
+  }
+}
+
+/**
+ * 变更主题
+ * @param {*} theme 主题的标记
+ */
+const changeTheme = (theme) => {
+  // html 的 class
+  let themeClassName = ''
+  switch (theme) {
+    case THEME_LIGHT:
+      themeClassName = 'light'
+      break
+    case THEME_DARK:
+      themeClassName = 'dark'
+      break
+      break
+    case THEME_SYSTEM:
+      // 调用方法，监听系统主题变化
+      watchSystemThemeChange()
+      themeClassName = matchMedia.matches ? 'dark' : 'light'
+      break
+  }
+  document.querySelector('html').className = themeClassName
+}
 /**
  * 初始化主题
  */
 export default () => {
-  // 1.当主题发生改变时，或者当今日系统时，可以进行 html class 的配置
-  watch(
-    () => store.getters.themeType,
-    (val) => {
-      // html 的 class
-      let themeClassName = ''
-      switch (val) {
-        case THEME_LIGHT:
-          themeClassName = 'light'
-          break
-        case THEME_DARK:
-          themeClassName = 'dark'
-          break
-      }
-      console.log(store.state.themeType)
-      document.querySelector('html').className = themeClassName
-    }
-  ),
+  // 1.当主题发生改变时，或者当进入系统时，可以进行 html class 的配置
+  watch(() => store.getters.themeType, changeTheme),
     {
       // 初始执行一次
       immediate: true
