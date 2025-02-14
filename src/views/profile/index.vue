071c75f1c53e45cf49248dedd1855c4224e7c0cf
toypipi <275688469@qq.com>
2025-02-14 16:21:01 +0800

    完成个人资料全部样式

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -8,28 +8,75 @@
       <!-- PC 端标题 -->
       <div v-else class="text-lg font-bold text-center mb-4 dark:text-zinc-300">个人资料</div>
       <div class="h-full w-full xl:w-2/3 px-1 pb-4 text-sm mt-2 xl:text-center">
-        <!-- 我的头像 -->
-        <span class="w-8 inline-block mb-2 font-black text-sm dark:text-zinc-300 xl:block xl:mx-auto">我的头像</span>
-        <div class="relative w-[80px] h-[80px] group xl:cursor-pointer xl:left-1/2 xl:translate-x-[-50%]">
-          <!-- 头像 -->
-          <img class="rounded-full w-full h-full xl:inline-block" :src="$store.getters.userInfo.avatar" alt="" />
-          <!-- 鼠标移入 -->
-          <div class="absolute top-0 rounded-full w-full h-full bg-black/40 hidden xl:group-hover:block">
-            <m-svg-icon name="change-header-image" class="w-2 h-2 m-auto mt-2"></m-svg-icon>
-            <div class="text-xs text-white dark:text-zinc-300 scale-90 mt-0.5">点击更换头像</div>
+        <!-- 头像部分 -->
+        <div class="py-1 xl:absolute xl:right-[16%] xl:text-center">
+          <span class="w-8 inline-block mb-2 font-black text-sm dark:text-zinc-300 xl:block xl:mx-auto">我的头像</span>
+          <div
+            class="relative w-[80px] h-[80px] group xl:cursor-pointer xl:left-1/2 xl:translate-x-[-50%]"
+            @click="onAvatarClick"
+          >
+            <!-- 头像 -->
+            <img class="rounded-full w-full h-full xl:inline-block" :src="$store.getters.userInfo.avatar" alt="" />
+            <!-- 鼠标移入 -->
+            <div class="absolute top-0 rounded-full w-full h-full bg-black/40 hidden xl:group-hover:block">
+              <m-svg-icon name="change-header-image" class="w-2 h-2 m-auto mt-2"></m-svg-icon>
+              <div class="text-xs text-white dark:text-zinc-300 scale-90 mt-0.5">点击更换头像</div>
+            </div>
           </div>
+          <!-- 隐藏域 -->
+          <input
+            v-show="false"
+            ref="inputFileTarget"
+            type="file"
+            accept=".png,.jpg,.jpeg,.gif"
+            @change="onSelectImgHandler"
+          />
+          <p class="mt-1 text-zinc-500 dark:text-zinc-400 text-xs xl:w-10">
+            支持 jpg、png、jpeg、gif 格式大小 1M 以内的图片
+          </p>
         </div>
-        <!-- 隐藏域 -->
-        <input
-          v-show="false"
-          ref="inputFileTarget"
-          type="file"
-          accept=".png,.jpg,.jpeg,.gif"
-          @change="onSelectImgHandler"
-        />
-        <p class="mt-1 text-zinc-500 dark:text-zinc-400 text-xs xl:w-10">
-          支持 jpg、png、jpeg、gif 格式大小 1M 以内的图片
-        </p>
+        <!-- 信息输入 -->
+        <!-- 用户名 -->
+        <div class="py-1 xl:flex xl:items-center xl:my-1">
+          <span class="w-8 block mb-1 font-bold dark:text-zinc-300 xl:mb-0">用户名</span>
+          <m-input class="w-full" max="20" v-model="$store.getters.userInfo.nickname"></m-input>
+        </div>
+        <!-- 职位 -->
+        <div class="py-1 xl:flex xl:items-center xl:my-1">
+          <span class="w-8 block mb-1 font-bold dark:text-zinc-300 xl:mb-0">职位</span>
+          <m-input class="w-full" v-model="$store.getters.userInfo.title"></m-input>
+        </div>
+        <!-- 公司 -->
+        <div class="py-1 xl:flex xl:items-center xl:my-1">
+          <span class="w-8 block mb-1 font-bold dark:text-zinc-300 xl:mb-0">公司</span>
+          <m-input class="w-full" v-model="$store.getters.userInfo.company"></m-input>
+        </div>
+        <!-- 个人主页 -->
+        <div class="py-1 xl:flex xl:items-center xl:my-1">
+          <span class="w-8 block mb-1 font-bold dark:text-zinc-300 xl:mb-0">个人主页</span>
+          <m-input class="w-full" v-model="$store.getters.userInfo.homePage"></m-input>
+        </div>
+        <!-- 个人介绍 -->
+        <div class="py-1 xl:flex xl:items-center xl:my-1">
+          <span class="w-8 block mb-1 font-bold dark:text-zinc-300 xl:mb-0">个人介绍</span>
+          <m-input class="w-full" v-model="$store.getters.userInfo.introduction" type="textarea" max="100"></m-input>
+        </div>
+
+        <!-- 保存修改 -->
+        <m-button
+          class="w-full mt-2 mb-4 dark:text-zinc-300 dark:bg-zinc-800 xl:w-[160px] xl:ml-[50%] xl:translate-x-[-50%]"
+        >
+          保存修改
+        </m-button>
+
+        <!-- 移动端：退出登录 -->
+        <m-button
+          v-if="isMobileTerminal"
+          class="w-full dark:text-zinc-300 dark:bg-zinc-800 xl:w-[160px] xl:ml-[50%] xl:translate-x-[-50%]"
+          @click="onLogoutClick"
+        >
+          退出登录
+        </m-button>
       </div>
     </div>
   </div>
@@ -37,9 +84,29 @@
 
 <script setup>
 import { isMobileTerminal } from '@/utils/flexible'
-// 移动端下 navbar
-const onNavbarLeftClick = () => {}
+import { confirm } from '@/libs'
+import { ref } from 'vue'
+import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 
+const store = useStore()
+const router = useRouter()
+
+// 移动端下 navbar
+const onNavbarLeftClick = () => {
+  router.back()
+}
+// 移动端退出登录
+const onLogoutClick = () => {
+  confirm('确定要退出登录吗？').then(() => {
+    store.dispatch('user/logout')
+  })
+}
+// 选择头像
+const inputFileTarget = ref(null)
+const onAvatarClick = () => {
+  inputFileTarget.value.click()
+}
 // 选中文件之后的回调
 const onSelectImgHandler = () => {}
 </script>
